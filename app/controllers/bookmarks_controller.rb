209fcead828f3b4774e999570b1717aa99e6ee71
Antoine Berson <antoine.berson01@gmail.com>
2022-11-18 08:57:13 +0100

    wip

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,22 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.save
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to bookmarks_path, status: :see_other
+  end
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie, :list)
+  end
 end
